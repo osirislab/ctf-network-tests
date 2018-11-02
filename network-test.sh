faca88bbdab3c64cbd3441fca67aec4b90b632e5
Leon Chou <leon.chou@nyu.edu>
2018-11-02 17:03:49 -0400

    servicce

--- network-test.sh
+++ network-test.sh
@@ -2,7 +2,7 @@
 
 for box in rev.chal.csaw.io web.chal.csaw.io pwn.chal.csaw.io crypto.chal.csaw.io misc.chal.csaw.io; do
     echo "Ok, testing $box now!";
-    result="$(nc $box 9000)";
+    result="$(echo 'PING' | nc $box 9000)";
     if [ -z "$result" ]; then
         echo "$box is not working :(";
     else
